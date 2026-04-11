59c1af29a916b8cce8d60735ccea0b76f937cfac
shiliu <982797454@qq.com>
2026-04-11 21:14:20 +0800

    添加演示工程。

--- Demos/CommonClassLibrary/IFeishuAppManager.cs
+++ Demos/CommonClassLibrary/IFeishuAppManager.cs
@@ -5,13 +5,18 @@
 //  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 // -----------------------------------------------------------------------
 
-namespace HttpClientApiTest.Api.Internal;
+using Mud.HttpUtils;
 
-partial class DingTalkDeptApi : IDingTalkDeptApi
+namespace CommonClassLibrary;
+
+
+public interface IFeishuAppManager
 {
-    public async Task<SysDeptInfoOutput> GetDeptXXXAsync([Token("TenantAccessToken")][Header("X-API-Key")] string apiKey, [Path] string? id)
-    {
+    IMudAppContext GetDefaultApp();
 
-        return null;
-    }
+    ITokenManager GetTokenManager(string tokenType);
+
+    IEnhancedHttpClient GetHttpClient();
+
+    IMudAppContext GetApp(string appKey);
 }
--- Demos/CommonClassLibrary/IFeishuEventDeduplicator.cs
+++ Demos/CommonClassLibrary/IFeishuEventDeduplicator.cs
@@ -5,13 +5,8 @@
 //  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 // -----------------------------------------------------------------------
 
-namespace HttpClientApiTest.Api.Internal;
+namespace CommonClassLibrary;
 
-partial class DingTalkDeptApi : IDingTalkDeptApi
+public interface IFeishuEventDeduplicator
 {
-    public async Task<SysDeptInfoOutput> GetDeptXXXAsync([Token("TenantAccessToken")][Header("X-API-Key")] string apiKey, [Path] string? id)
-    {
-
-        return null;
-    }
 }
