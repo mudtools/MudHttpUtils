feea9146972bdc7d2b4f8b4f4604ff8a36368caa
倔强的石头 <413901125@qq.com>
2026-04-21 16:14:06 +0800

    feat: 添加基准测试项目并优化AOT兼容性
    添加Mud.HttpUtils.Benchmarks基准测试项目
    为Mud.HttpUtils相关项目启用AOT分析器
    更新.gitignore忽略基准测试项目的生成目录
    移除过时的NamingHelperTests测试文件
    添加Verify测试相关依赖

--- Tests/Mud.HttpUtils.Generator.Tests/GlobalUsings.cs
+++ Tests/Mud.HttpUtils.Generator.Tests/GlobalUsings.cs
@@ -13,6 +13,7 @@ global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using System;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
+global using System.IO;
 global using System.Linq;
 global using System.Text;
 global using System.Threading;
